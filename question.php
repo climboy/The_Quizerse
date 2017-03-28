ceb7b1b18501e4288a1c5c48cce1faae9eedb25b
climboy <thomas.clavier70@yahoo.fr>
2017-03-28 20:10:58 +0200

    The_Quizerse
    modification

---
+++ question.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/question.css">
+    <link href="https://fonts.googleapis.com/css?family=Passion+One" rel="stylesheet">
+    <title>Question</title>
+  </head>
+  <body>
+<div class="container">
+  <p>Question 1</p>
+</div>
+  </div>
+  </body>
+</html>
