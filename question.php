33b5cb090641718d44cee93585b25c25354cb0f5
Zorath <brimwen@gmail.com>
2017-03-28 22:42:51 +0200

    Zoom de la question Affichage du texte sur l'introduction Redirection vers l'index au clic dans l'introduction

--- question.php
+++ question.php
@@ -6,11 +6,28 @@
     <link rel="stylesheet" href="css/question.css">
     <link href="https://fonts.googleapis.com/css?family=Passion+One" rel="stylesheet">
     <title>Question</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js"></script>
+    <script
+			  src="https://code.jquery.com/jquery-3.1.1.min.js"
+			  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+			  crossorigin="anonymous"></script>
+    <script type="text/javascript">
+    	$(document).ready(function(){
+			// set transform origin to top left
+			TweenMax.set('.container', {
+				visibility: "visible",
+				scale: 0,
+				transformOrigin:"50% 50%"
+			}); 
+			// since scaleX and scaleY are the same you can just use scale
+			TweenMax.to('.container', 1, {scale:1}); 
+		});
+    </script>
   </head>
   <body>
-<div class="container">
-  <p>Question 1</p>
-</div>
+	<div class="container">
+	  <p>Question 1</p>
+	</div>
   </div>
   </body>
 </html>
